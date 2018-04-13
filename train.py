bc2f25d55f364b68bc802a792d4b4a4d31bb566f
Joonyoung Yi <joonyoung.yi@kaist.ac.kr>
2018-04-13 14:55:12 +0900

    run skeleton code

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import shutil
 
 import tensorflow as tf
 
-
 # Hyperparameters
 input_layer_size = 784
 hidden_layer_size = 100
@@ -22,6 +21,13 @@ y = tf.placeholder(tf.int32, [None], 'y')
 
 hidden = tf.layers.dense(x, hidden_layer_size, activation=tf.nn.relu)
 output = tf.layers.dense(hidden, output_layer_size)
+
+print(x)
+print(hidden)
+print(output)
+print(y)
+assert False
+# print()
 
 pred = tf.argmax(output, axis=1, output_type=tf.int32)
 acc = tf.reduce_mean(tf.cast(tf.equal(pred, y), tf.float32))
@@ -44,11 +50,16 @@ saver = tf.train.Saver()
 # Training iteration
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-for i in range(1, num_iters+1):
+
+for i in range(1, num_iters + 1):
     batch_x, batch_y = mnist.train.next_batch(batch_size)
 
-    fetch_dict = {'loss': loss, 'acc': acc, 'train': train_op,
-                  'summary': summ_op}
+    fetch_dict = {
+        'loss': loss,
+        'acc': acc,
+        'train': train_op,
+        'summary': summ_op
+    }
     feed_dict = {x: batch_x, y: batch_y}
 
     result = sess.run(fetch_dict, feed_dict)
@@ -65,8 +76,8 @@ for i in range(1, num_iters+1):
 
         result = sess.run(fetch_dict, feed_dict)
 
-        print('valid at step', i,
-              'acc=%.3f, loss=%.3f' % (result['acc'], result['loss']))
+        print('valid at step', i, 'acc=%.3f, loss=%.3f' % (result['acc'],
+                                                           result['loss']))
         valid_writer.add_summary(result['summary'], i)
         saver.save(sess, './logs/ckpt/model-%d.ckpt' % (i))
 
